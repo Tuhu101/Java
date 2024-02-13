41b7218855017c95a2dbab22a78e67c58ab8351b
Tobi8570 <73465176+Tobi8570@users.noreply.github.com>
2024-02-13 12:14:29 +0100

    Formattet documents

--- MainMenu.java
+++ MainMenu.java
@@ -2,24 +2,33 @@ import java.util.Scanner;
 
 public class MainMenu {
     static void menu() {
-    Main.clearScreen();
-    int userChoice;
-    System.out.println("Welcome to the program!");
-    System.out.println("What part of the program would you like to use?\n");
+        Main.clearScreen();
+        int userChoice;
+        System.out.println("Welcome to the program!");
+        System.out.println("What part of the program would you like to use?\n");
 
-    Scanner input = new Scanner(System.in);
-    System.out.println("SimpleCalculator(1)");
+        Scanner input = new Scanner(System.in);
+        System.out.println("SimpleCalculator(1)");
 
-    System.out.print("Choice: ");
-    userChoice = input.nextInt();
+        System.out.print("Choice: ");
+        userChoice = input.nextInt();
 
-    switch (userChoice) {
-      case 1:
-        simpleCalculator.Calculator();
-        break;
+        switch (userChoice) {
+            case 1:
+                simpleCalculator.Calculator();
+                break;
+            case 2:
+                simpleCalculator.Calculator();
+                break;
+            case 3:
+                simpleCalculator.Calculator();
+                break;
+            case 4:
+                simpleCalculator.Calculator();
+                break;
 
-      default:
-        System.out.println("Invalid input");
+            default:
+                System.out.println("Invalid input");
+        }
     }
-  }
 }
